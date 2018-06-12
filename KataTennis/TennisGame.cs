20df9819de21016f93d55f625cb612a13e7bba9e
Vajkay René <vajkay.rene@gmail.com>
2018-06-12 15:08:38 +0200

    Implements basic TennisGame methods and TennisGameState properties

--- KataTennis/TennisGame.cs
+++ KataTennis/TennisGame.cs
@@ -2,7 +2,24 @@ namespace KataTennis
 {
     public class TennisGame
     {
+        readonly TennisGameState _state;
+
         public TennisGame()
+            : this(new TennisGameState())
+        {
+        }
+
+        public TennisGame(TennisGameState state)
+        {
+            _state = state;
+        }
+
+        public Score GetScore(Player Player)
+        {
+            return Score.Love;
+        }
+
+        public void PromoteScore(Player Player)
         {
             
         }
--- KataTennis/TennisGameState.cs
+++ KataTennis/TennisGameState.cs
@@ -2,9 +2,8 @@ namespace KataTennis
 {
     public class TennisGameState
     {
-        public TennisGameState()
-        {
-            
-        }
+        public Score PlayerA { get; set; } = Score.Love;
+
+        public Score PlayerB { get; set; } = Score.Love;
     }
 }
