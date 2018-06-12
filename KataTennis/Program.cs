2e98b1bae0fdc74eaa0f7e5b35d44f5f3ab0ac9b
Vajkay René <vajkay.rene@gmail.com>
2018-06-12 14:33:31 +0200

    Initial commit of the KataTennis project

---
+++ KataTennis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataTennis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
