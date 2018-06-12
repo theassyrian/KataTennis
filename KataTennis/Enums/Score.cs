96b569ddccdcf5988a35f111f1b931092e9e2012
Vajkay René <vajkay.rene@gmail.com>
2018-06-12 16:01:42 +0200

    Implements PromoteScore method

--- KataTennis/Enums/Score.cs
+++ KataTennis/Enums/Score.cs
@@ -2,11 +2,11 @@ namespace KataTennis
 {
     public enum Score
     {
-        Love,
-        Fifteen,
-        Thirty,
-        Forty,
-        Advantage,
-        Won
+        Love = 0,
+        Fifteen = 1,
+        Thirty = 2,
+        Forty = 3,
+        FortyWithAdvantage = 4,
+        Won = 5
     }
 }
--- KataTennis/TennisGame.cs
+++ KataTennis/TennisGame.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace KataTennis
 {
     public class TennisGame
@@ -16,14 +19,60 @@ namespace KataTennis
 
         public Score GetScore(Player player) 
         {
-            return IsPlayerA(player) ? _state.PlayerA : _state.PlayerB;
+            return GetPlayerScore(player);
         }
 
-        public void PromoteScore(Player Player)
+        public void PromoteScore(Player player)
         {
+            var score = GetPlayerScore(player);
             
+            if (IsFinishing(score))
+            {
+                PromoteFinishingScore(player, score);
+            }
+            else
+            {
+                SetPlayerScore(player, score + 1);
+            }
         }
 
-        private bool IsPlayerA(Player player) => player == Player.A;
+        private void PromoteFinishingScore(Player player, Score playerScore)
+        {
+            var otherPlayerScore = GetOtherPlayerScore(player);
+
+            if (HasNoAdvantage(playerScore) && IsFinishing(otherPlayerScore))
+            {
+                SetPlayerScore(player, Score.FortyWithAdvantage);
+                SetOtherPlayerScore(player, Score.Forty);
+            }
+            else
+            {
+                SetPlayerScore(player, Score.Won);
+            }
+        }
+
+        private Score GetPlayerScore(Player player) => GetScore(() => player == Player.A);
+
+        private Score GetOtherPlayerScore(Player player) => GetScore(() => player != Player.A);
+
+        private Score GetScore(Func<bool> isPlayerA) => isPlayerA() ? _state.PlayerA : _state.PlayerB;
+        
+        private void SetPlayerScore(Player player, int score) => SetPlayerScore(player, (Score)score);
+
+        private void SetPlayerScore(Player player, Score score) => SetScore(() => player == Player.A, score);
+
+        private void SetOtherPlayerScore(Player player, Score score) => SetScore(() => player != Player.A, score);
+
+        private void SetScore(Func<bool> isPlayerA, Score score)
+        {
+            if(isPlayerA())
+                _state.PlayerA = score;
+            else
+                _state.PlayerB = score;
+        }
+
+        private bool IsFinishing(Score score) => score >= Score.Forty;
+
+        private bool HasNoAdvantage(Score score) => score != Score.FortyWithAdvantage;
     }
 }
